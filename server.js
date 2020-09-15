ca2af470e39620b20859b816d5fe66a7e5c038d9
BrenoOsvaldoFunicheli <osvaldobreno99@gmail.com>
2020-09-15 13:53:27 -0300

    Set up

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const nomeApp = process.env.npm_package_name;
 const app = express();
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-// app.use(express.static(`${__dirname}/dist/genexapp-ng`));
 
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-// res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 
 });
 
