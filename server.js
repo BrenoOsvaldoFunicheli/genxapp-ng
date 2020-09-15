1ebd8d128af0409c459fc300cf68f6837cc575c7
BrenoOsvaldoFunicheli <osvaldobreno99@gmail.com>
2020-09-15 13:56:15 -0300

    Set up

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-
 });
 
 app.listen(process.env.PORT || 8080);
